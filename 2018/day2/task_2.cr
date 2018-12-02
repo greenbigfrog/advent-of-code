44e80dbffd014c5a4771e1abcac691c4592dad49
Jonathan B <greenbigfrog@gmail.com>
2018-12-02 13:19:48 +0100

    Improve coding style

--- 2018/day2/task_2.cr
+++ 2018/day2/task_2.cr
@@ -4,22 +4,20 @@ input = File.read_lines("input.txt")
 chars = ""
 continue = true
 input.each do |outer_line|
-  if continue
-    input.each do |inner_line|
-      if continue
-        next if inner_line == outer_line
-        count = 0
-        inner_line.each_char_with_index do |char, index|
-          break unless continue
-          if outer_line[index] == char
-            count += 1
-          end
-          if count >= 25
-            char = inner_line.chars - outer_line.chars
-            chars = inner_line.delete(char[0])
-            continue = false
-          end
-        end
+  break unless continue
+  input.each do |inner_line|
+    break unless continue
+    next if inner_line == outer_line
+    count = 0
+    inner_line.each_char_with_index do |char, index|
+      break unless continue
+      if outer_line[index] == char
+        count += 1
+      end
+      if count >= 25
+        char = inner_line.chars - outer_line.chars
+        chars = inner_line.delete(char[0])
+        continue = false
       end
     end
   end
