6b3f1ed3492910018105558b439705a001780669
Jonathan B <greenbigfrog@gmail.com>
2018-12-05 23:15:52 +0100

    Improve day 5 by a lot

--- 2018/day5/task.cr
+++ 2018/day5/task.cr
@@ -2,65 +2,45 @@ require "benchmark"
 
 input = File.read_lines("input.txt")
 
-input = ["dabAcCaCBAcCcaDA"]
+# input = ["dabAcCaCBAcCcaDA"]
 
-def delete(array : Array(Char) | Array(Char?))
-  puts array.join("")
-  copy = Array(Char).new
-  prev = ' '
-  skip = false
-  array.each_with_index do |char, i|
-    copy << char
-    next unless char.is_a?(Char)
-    next prev = char if i == 0
-    if prev.downcase == char.downcase
-      next skip = false if skip
-      if char.lowercase? == prev.lowercase? # check if same case
-        skip = false
-      else
-        copy.pop?
-        copy.pop?
-        skip = true
-      end
-    else
-      skip = false
+DIFF = ('A' - 'a').abs
+
+def task_1(data : String | Array(Char))
+  if data.is_a?(String)
+    array = data.chars
+  else
+    array = data
+  end
+
+  i = 0
+  while i < (array.size - 1)
+    if (array[i + 1] - array[i]).abs == DIFF
+      array.delete_at(i, 2)
+      i -= 1 if i > 0
+      i -= 1 if i > 0
     end
-    prev = char
+    i += 1
   end
-  copy
-end
-
-def reduce(array : Array(Char))
-  loop do
-    new_array = delete(array.dup)
-    break if new_array == array
-    array = new_array
-  end
-  array
-end
-
-def task_1(string : String)
-  reduce(string.chars).size
+  array.size
 end
 
 def task_2(string : String)
   chars = ('a'..'z').to_a
   chars.min_of do |x|
-    puts string
-    size = reduce(string.dup.chars.reject { |i| i.downcase == x }).size
-    puts "#{x}: #{size}"
-    size
+    test = string.gsub(/#{x}/i, "")
+    task_1(test) - 2
   end
 end
 
 a = Time.measure do
   b = input[0]
-  # task_1(b)
+  pp task_1(b)
   pp task_2(b)
 end
 pp a.total_milliseconds
 
 # Benchmark.ips do |x|
-#   x.report("First attempt") { reduce(input[0].chars).size }
-#   x.report("Second attempt") { reduce_test(input[0].chars).size }
+#   x.report("part_1") { task_1(input[0]) }
+#   x.report("part_2") { task_2(input[0]) }
 # end
