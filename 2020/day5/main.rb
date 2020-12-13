a8cf29c92a48d1fc357516b2b37c499c6eda8bb2
Jonathan B <greenbigfrog@gmail.com>
2020-12-13 17:28:25 +0100

    Day 5

---
+++ 2020/day5/main.rb
@@ -0,0 +1,56 @@
+sample = [
+  "BFFFBBFRRR",
+  "FFFBBBFRRR",
+  "BBFFBBFRLL"
+]
+
+# input = sample
+# input = ["FBFBBFFRLR"]
+
+input = File.readlines("input.txt")
+
+ids = []
+max = 0
+
+
+input.each do |i|
+  low = 0
+  high = 127
+  row = 0
+  low_c = 0
+  high_c = 7
+  col = 0
+  i.each_char do |c|
+    half = (high - low + 1) / 2
+    half_c = (high_c - low_c + 1) / 2
+    case c
+    when "F" 
+      high -= half
+      row = low
+    when "B" 
+      low += half
+      row = high
+    when "L"
+      high_c -= half_c
+      col = low_c
+    when "R"
+      low_c += half_c
+      col = high_c
+    end
+    # puts "Low: #{low_c} High: #{high_c}"
+  end
+  id = row * 8 + col
+  # puts "Row: #{row} Col: #{col} ID: #{id}"
+
+  ids << id
+
+  max = id if id > max
+end
+
+puts "Part 1: #{max}"
+
+ids.sort!.each_with_index do |x, i|
+  if ids[i-1] == x-2
+    puts "Part 2: #{x}"
+  end
+end
