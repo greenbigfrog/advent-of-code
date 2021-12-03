89d8ad45bedd9ecfbfecfd88a0d76413bb8c942f
greenbigfrog <greenbigfrog@gmail.com>
2021-12-03 19:03:20 +0100

    continue

--- 2021/day3/day3.rb
+++ 2021/day3/day3.rb
@@ -1,5 +1,6 @@
-size = 12
-input = File.readlines("input.txt")
+size = 5
+file = "example.txt"
+input = File.readlines(file)
 map = {}
 map["0"] = Array.new(12, 0)
 map["1"] = Array.new(12, 0)
@@ -19,3 +20,27 @@ end
 out = out >> 1
 epsi = out ^ (1 << size) - 1
 puts "Gamma: #{out} Epsilon: #{epsi} Result: #{out * epsi}"
+
+final = input.dup
+input.each_with_index do |l, index|
+    stop = false
+    size.times do |i|
+        next if stop
+        check = nil
+        if map["0"][i] > map["1"][i]
+            check = "0"
+        else
+            check = "1"
+        end
+        puts "looking in index #{i}"
+        if l.split("")[i] == check
+            nil
+        else
+            puts "throwing out #{l}"
+            final[index] = nil
+            stop = true
+        end
+    end
+end
+
+puts final.uniq.inspect
