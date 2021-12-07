845ff23492184700bc8f10501cc890e42e4879d9
greenbigfrog <greenbigfrog@gmail.com>
2021-12-07 13:30:35 +0100

    switch to average for offset for part2

--- 2021/day7/day7.rb
+++ 2021/day7/day7.rb
@@ -5,24 +5,14 @@ input = t.split(',').map { |x| x.to_i }
 sorted = input.sort
 puts "Offset: #{offset = sorted[input.size/2]}"
 sum = sorted.map { |x| (x - offset).abs }.sum
-puts sum
+puts "Part 1: #{sum}"
 
 
 # Part 2
-# This only works semi well, and is more or less just bruteforcing the solution,
-# except skipping to a sensible area to check
-exp = 100000000000 - 1
-last = 100000000000
-while exp < last
-    last = exp
-    puts "offset: #{offset}"
-    exp = sorted.map do |x| 
-        x = (x - offset).abs 
-        r = (x * (x+1))/2
-        # puts "x: #{x} r: #{r}"
-        r
-    end.sum
-    puts "exp: #{exp}"
-    offset += 1
-end
-puts exp
+offset = sorted.sum / sorted.size
+puts "Offset: #{offset}"
+exp = sorted.map do |x| 
+    x = (x - offset).abs 
+    (x * (x+1))/2
+end.sum
+puts "Part 2: #{exp}"
