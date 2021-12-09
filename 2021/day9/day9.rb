4f51169f627d0bbc4a9844c56b2b8d1898fcddbf
greenbigfrog <greenbigfrog@gmail.com>
2021-12-09 20:39:52 +0100

    part 1

--- 2021/day9/day9.rb
+++ 2021/day9/day9.rb
@@ -1,9 +1,32 @@
-file = File.readlines("example.txt")
+file = File.readlines("input.txt")
 
-map = Array.new
+map = []
 file.each_with_index do |line, i|
     map[i] = line.rstrip.split("").map { |x| x.to_i }
 end
 
-lines = []
-pp map
+def check(map, comp, x, y)
+    left = x-1 < 0 ? 100 : map[x-1][y].to_i
+    right = x+1 >= map.size ? 100 : map[x+1][y].to_i
+    row = left > comp && comp < right 
+    
+    w = map[x]
+    above = y-1 < 0 ? 100 : w[y-1].to_i
+    below = y+1 >= map.first.size ? 100 : w[y+1].to_i
+    column = above > comp && comp < below
+
+    row && column
+end
+
+sum = 0
+map.size.times do |x|
+    map.first.size.times do |y|
+        comp = map[x][y]
+        if check(map, comp, x, y)
+            # puts "added #{x} #{y}: #{comp}"
+            sum += comp + 1
+        end
+    end
+end
+
+puts sum
